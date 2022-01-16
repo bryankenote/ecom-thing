0b74b79feb912f69b7d34422f103390e6a9bbd72
ebingeb <ebingerbradley@gmail.com>
2022-01-16 09:43:25 -0800

    make additional classname changes and styling changes

--- client/src/components/Login/login.tsx
+++ client/src/components/Login/login.tsx
@@ -15,23 +15,23 @@ function Login() {
 
 	return (
 		<div>
-			<div className="logindiv">
+			<div className="logindiv1">
 				<label className="loginLabel">
 					<b>Login</b>
 				</label>
 			</div>
 
-			<div className="logindiv">
+			<div className="logindiv2">
 				<label>Username</label>
 				<input type="text" onChange={handleUserChange} />
 			</div>
 
-			<div className="logindiv">
+			<div className="logindiv2">
 				<label>Password</label>
 				<input type="password" onChange={handlePasswordChange} />
 			</div>
 
-			<div className="logindiv">
+			<div className="logindiv2">
 				<Link to="/catalog">Submit</Link>
 			</div>
 		</div>
