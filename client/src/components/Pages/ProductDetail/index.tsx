4779137280ac0cbc40952e35ecb0103a9636e411
ebingeb <ebingerbradley@gmail.com>
2022-01-27 09:31:42 -0800

    add addtocart ui and quantity input

--- client/src/components/Pages/ProductDetail/index.tsx
+++ client/src/components/Pages/ProductDetail/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
-import Product from '../../Generic/Product';
+import { LocalizationContext } from '../../../localization';
+import Product, { product } from '../../Generic/Product';
+import style from './style.module.css';
 
+interface IProductDetail {
+	onAdd: (product: product, quantity: number) => void;
+}
 interface ILocationState {
-	item: {
-		title: string;
-		img: string;
-		price: number;
-	};
+	item: product;
 }
 
-function ProductDetail() {
+function ProductDetail(props: IProductDetail) {
 	const location = useLocation();
 	const { item } = location.state as ILocationState;
-	return <Product item={item} />;
+	const strings = useContext(LocalizationContext);
+
+	const [itemQuantity, setItemQuantity] = useState<number>(1);
+
+	const handleItemQuantityChange = (event) => {
+		const quantity = parseInt(event.target.value);
+		if (quantity === 0) {
+			return;
+		} else {
+			setItemQuantity(quantity);
+		}
+	};
+
+	const addToCart = () => {
+		props.onAdd(item, itemQuantity);
+	};
+
+	return (
+		<div>
+			<Product item={item} />
+			<button className={style.addToCart} onClick={addToCart}>
+				{strings.addToCart}
+			</button>
+			<input
+				className={style.input}
+				type="number"
+				value={itemQuantity}
+				onChange={handleItemQuantityChange}
+			></input>
+		</div>
+	);
 }
 
 export default ProductDetail;
