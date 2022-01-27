2a48c542854a853d2e68146ca311563cfaf37d19
ebingeb <ebingerbradley@gmail.com>
2022-01-27 09:26:17 -0800

    add checkout page

---
+++ client/src/components/Pages/Checkout/index.tsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { LocalizationContext } from '../../../localization';
+import { ICart } from '../../../App';
+import Product from '../../Generic/Product';
+import style from './style.module.css';
+
+interface ICheckout {
+	cartItems: ICart;
+}
+
+function Checkout({ cartItems }: ICheckout) {
+	const strings = useContext(LocalizationContext);
+
+	return (
+		<div>
+			<div>
+				<h1>{strings.shoppingCart}</h1>
+				<div className={style.border}></div>
+			</div>
+			<div>
+				{Object.values(cartItems).map((value, index) => (
+					<div className={strings.checkoutItem} key={index}>
+						<Product item={value.product} />
+						<h3 className={strings.checkoutItemQty}>
+							Quantity: {value.quantity}
+						</h3>
+						<div className={style.border}></div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Checkout;
