35fd33b0cde813d4778f09db36a1aa6dfa9c301f
ebingeb <ebingerbradley@gmail.com>
2022-01-27 09:27:32 -0800

    add product type

--- client/src/components/Generic/Product/index.tsx
+++ client/src/components/Generic/Product/index.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { LocalizationContext } from '../../../localization';
 
+export type product = {
+	title: string;
+	img: string;
+	price: number;
+};
 interface IProduct {
-	item: {
-		title: string;
-		img: string;
-		price: number;
-	};
+	item: product;
 }
 
 function Product(props: IProduct) {
@@ -14,7 +15,7 @@ function Product(props: IProduct) {
 	return (
 		<div>
 			<h2>{props.item.title}</h2>
-			<img src={props.item.img} alt="" />
+			<img src={props.item.img} alt={props.item.title} />
 			<h3>
 				{strings.price}: {props.item.price}
 			</h3>
