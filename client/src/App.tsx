82fe3fa02caad7b4e6417ba2520279d1efb0647c
ebingeb <ebingerbradley@gmail.com>
2022-01-27 09:33:15 -0800

    add shopping cart state and onadd handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useState, useContext, useCallback } from 'react';
 import Catalog from './components/Pages/Catalog';
-import Product from './components/Pages/ProductDetail';
 import Login from './components/Pages/Login';
 import { Routes, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 import { LocalizationContext, localStrings } from './localization';
+import Checkout from './components/Pages/Checkout';
 import './App.css';
+import ProductDetail from './components/Pages/ProductDetail';
+import { product } from './components/Generic/Product';
+
+export interface ICart {
+	[title: string]: {
+		product: product;
+		quantity: number;
+	};
+}
 
 function App() {
+	const strings = useContext(LocalizationContext);
+
+	const [cartItems, setCartItems] = useState<ICart>({});
+
+	const onAdd = useCallback((product: product, quantity: number) => {
+		setCartItems((cartItems) => {
+			let cartItem = cartItems[product.title];
+			let newQuantity;
+			if (cartItem) {
+				newQuantity = quantity + cartItem.quantity;
+			} else {
+				newQuantity = quantity;
+			}
+			return {
+				...cartItems,
+				[product.title]: { product, quantity: newQuantity },
+			};
+		});
+	}, []);
+
 	return (
 		<LocalizationContext.Provider value={localStrings}>
 			<div className="App">
+				<Link to="/Checkout">{strings.checkout}</Link>
 				<Routes>
 					<Route path="/" element={<Login />} />
 					<Route path="/catalog" element={<Catalog />} />
-					<Route path="/product" element={<Product />} />
+					<Route
+						path="/product"
+						element={<ProductDetail onAdd={onAdd} />}
+					/>
+					<Route
+						path="/checkout"
+						element={<Checkout cartItems={cartItems} />}
+					/>
 				</Routes>
 			</div>
 		</LocalizationContext.Provider>
