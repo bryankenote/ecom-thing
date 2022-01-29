dc74785f51aad45b056a33c53bdc0b8f3adc2a8b
ebingeb <ebingerbradley@gmail.com>
2022-01-29 10:12:57 -0800

    fix top bar issue where it appears on login page

--- client/src/TopBar/index.tsx
+++ client/src/TopBar/index.tsx
@@ -1,16 +1,20 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { LocalizationContext } from '../localization';
 import style from './style.module.css';
 
+const TOP_BAR_PATHS = new Set(['/catalog', '/product', '/checkout']);
+
 function TopBar() {
 	const strings = useContext(LocalizationContext);
-	return (
+	const location = useLocation();
+	return TOP_BAR_PATHS.has(location.pathname) ? (
 		<div className={style.TopContainer}>
 			<Link to="/catalog">{strings.catalog}</Link>
 			<Link to="/checkout">{strings.checkout}</Link>
 		</div>
-	);
+	) : null;
 }
 
 export default TopBar;
