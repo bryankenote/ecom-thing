517b2f248ae764929526851029610fa9526b9cfb
ebingeb <ebingerbradley@gmail.com>
2022-02-01 09:21:01 -0800

    create delete button for checkout page

--- client/src/components/Pages/Checkout/index.tsx
+++ client/src/components/Pages/Checkout/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { LocalizationContext } from '../../../localization';
 import { ICart } from '../../../App';
-import Product, { product } from '../../Generic/Product';
+import Product from '../../Generic/Product';
 import style from './style.module.css';
 
 interface ICheckout {
@@ -13,17 +13,21 @@ function Checkout({ cartItems }: ICheckout) {
 
 	return (
 		<div>
-			<div>
-				<h1>{strings.shoppingCart}</h1>
-				<div className={style.border}></div>
-			</div>
+			<h1 className={style.shoppingCart}>{strings.shoppingCart}</h1>
 			<div>
 				{Object.values(cartItems).map((value, index) => (
-					<div className="checkoutItem" key={index}>
-						<Product item={value.product} />
-						<h3 className="checkout-item-qty">
-							{strings.quantity}: {value.quantity}
-						</h3>
+					<div className={style.checkoutItems} key={index}>
+						<div>
+							<Product item={value.product} />
+							<h3>
+								{strings.quantity}: {value.quantity}
+							</h3>
+						</div>
+						<div>
+							<button className={style.deleteButton}>
+								{strings.deleteItem}
+							</button>
+						</div>
 					</div>
 				))}
 			</div>
