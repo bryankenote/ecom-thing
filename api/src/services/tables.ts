8d8dace192a746824c5155610b1f6ce8f4076534
ebingeb <ebingerbradley@gmail.com>
2022-02-06 16:58:05 -0800

    change product sql table categories and data types

--- api/src/services/tables.ts
+++ api/src/services/tables.ts
@@ -9,7 +9,13 @@ export default {
 	products: `
 	CREATE TABLE IF NOT EXISTS products (
 	  id INT AUTO_INCREMENT PRIMARY KEY,
-	  number INT NOT NULL,
+	  title VARCHAR(255),
+	  price FLOAT(10, 2),
+	  description VARCHAR(255),
+	  category VARCHAR(255),
+	  image VARCHAR(255),
+	  rate DECIMAL(65, 1),
+	  count INT(255),
 	  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	)  ENGINE=INNODB;
 `,
