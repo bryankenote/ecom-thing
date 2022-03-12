befa64ea1674dbebaa243ede6abac3d0c14984f2
bryankenote <brykenote@gmail.com>
2022-03-11 21:06:46 -0800

    prevent duplicate db connections

--- api/src/services/db.ts
+++ api/src/services/db.ts
@@ -2,13 +2,15 @@ import mysql from 'mysql2/promise';
 import tables from './tables';
 import config from '../config';
 
+let connection: mysql.Connection;
+
 const init = async () => {
 	await Promise.all(Object.values(tables).map((sql) => query(sql)));
 	console.log('Finished intializing tables');
 };
 
 export async function query(sql: string, params?: { [param: string]: any }) {
-	const connection = await mysql.createConnection(config.db);
+	connection = connection ?? (await mysql.createConnection(config.db));
 	const [results] = await connection.query(sql, params);
 
 	return results;
